10b6636adaae57db9659a57ffc0036c02dafd9a0
nmys9 <141567968+nmys9@users.noreply.github.com>
2024-03-24 15:48:28 +0200

    Time: 0 ms (100%), Space: 12 MB (49.92%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -3,19 +3,17 @@ public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int m=matrix.size();
         int n=matrix[0].size();
-        int left=0;
-        int right=(n*m)-1;
-        int index,element;
+        int size=n*m;
+        int left=0, right=size-1;
         while(left<=right){
-            index=(left+right)/2;
-            element=matrix[index/n][index%n];
-            if(target==element){
+            int mid=(left+right)/2;
+            int r=mid/n, c=mid%n;
+            if(matrix[r][c]==target)
                 return true;
-            }else if(target<element){
-                right=index-1;
-            }else{
-                left=index+1;
-            }
+            else if(matrix[r][c]<target)
+                left=mid+1;
+            else
+                right=mid-1; 
         }
         return false;
     }
