97cdf386e8c733930a46d6fb8e0d2f47b78f75a7
nmys9 <141567968+nmys9@users.noreply.github.com>
2024-04-04 17:54:26 +0300

    Time: 422 ms (43.65%), Space: 21.8 MB (29.5%) - LeetHub

--- 2523-closest-prime-numbers-in-range/2523-closest-prime-numbers-in-range.cpp
+++ 2523-closest-prime-numbers-in-range/2523-closest-prime-numbers-in-range.cpp
@@ -1,41 +1,35 @@
 class Solution {
- public:
-  vector<int> closestPrimes(int left, int right) {
-    const vector<bool> isPrime = sieveEratosthenes(right + 1);
-    vector<int> primes;
+public:
+    vector<int> closestPrimes(int left, int right) {
+        int n=right+1;
+        vector<bool> isPrime(n,true);
+        isPrime[0]=isPrime[1]=false;
+        for(int i=2;i*i<=n;i++){
+            if(isPrime[i]){
+                for(int j=i*i;j<=n;j+=i)
+                    isPrime[j]=false;
+            }
+        }
+        vector<int> primes;
+        for(int i=left;i<=right;i++){
+            if(isPrime[i])primes.push_back(i);
+        }
 
-    for (int i = left; i <= right; ++i)
-      if (isPrime[i])
-        primes.push_back(i);
+        if(primes.size()<2){
+            return {-1,-1};
+        }
+        int min=INT_MAX;
+        int num1=-1,num2=-1;
+        for(int i=1;i<primes.size();i++){
+            int diff=primes[i]-primes[i-1];
+            if(diff<min){
+                min=diff;
+                num1=primes[i-1];
+                num2=primes[i];
+            }
 
-    if (primes.size() < 2)
-      return {-1, -1};
 
-    int minDiff = INT_MAX;
-    int num1 = -1;
-    int num2 = -1;
-
-    for (int i = 1; i < primes.size(); ++i) {
-      const int diff = primes[i] - primes[i - 1];
-      if (diff < minDiff) {
-        minDiff = diff;
-        num1 = primes[i - 1];
-        num2 = primes[i];
-      }
+        }
+    return {num1,num2};
     }
-
-    return {num1, num2};
-  }
-
- private:
-  vector<bool> sieveEratosthenes(int n) {
-    vector<bool> isPrime(n, true);
-    isPrime[0] = false;
-    isPrime[1] = false;
-    for (int i = 2; i * i < n; ++i)
-      if (isPrime[i])
-        for (int j = i * i; j < n; j += i)
-          isPrime[j] = false;
-    return isPrime;
-  }
 };
