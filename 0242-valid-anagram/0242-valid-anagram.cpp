450800bd1032bc92dc048bb0b5dfbbaee023955b
nmys9 <141567968+nmys9@users.noreply.github.com>
2024-04-04 23:11:40 +0300

    Time: 9 ms (50.84%), Space: 8.9 MB (39%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -2,8 +2,11 @@ class Solution {
 public:
     vector<int> frequencyArray(string arr){
         vector<int> freq(26,0);
-        for(int i=0;i<arr.size();i++)
-            freq[arr[i]-'a']++;
+        for(auto n:arr){
+            freq[n-'a']++;
+        }
+        // for(int i=0;i<arr.size();i++)
+        //     freq[arr[i]-'a']++;
         return freq;
     }
     bool isAnagram(string s, string t) {
